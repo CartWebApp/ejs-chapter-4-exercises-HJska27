f87e02370c81f09674b3a3b7a0c9da1ef43e31c1
HJska27 <kallvey.thong23@student.cart.org>
2024-02-02 09:09:02 -0800

    Update reverse-arrays.js

--- reverse-arrays.js
+++ reverse-arrays.js
@@ -1,13 +1,32 @@
 function reverseArray(arr) {
-  let reverseArray = ["A", "B", "C"];
-  reverseArray[0] = "C"
-  reverseArray[1] = "B"
-  reverseArray[2] = "A"
-console.log(reverseArray)
+  let reverseArray = [];
+  
+  // [ A, B , C]
+  //   0  1   2
+
+  // i=2 [C]
+  // i=1 [C, B]
+  // i=0 [C, B, A]
+  for(let i = arr.length -1; i >=0; i--){
+    reverseArray.push(arr[i]);
+  }
+return reverseArray
 }
 
 function reverseArrayInPlace(arr) {
-  // Add code.
+  // [1,2,3,4,5] start array
+  //  ^       ^
+  //  0       4
+  // [5,2,3,4,1] iteration 1
+  //    ^   ^
+  //    1   3  
+  // [5,4,3,2,1] iteration 2 / end
+  for(let i = 0; i < Math.floor(arr.length / 2); i++){
+    let tempVal = arr[i];
+    arr[i] = arr[arr.length - 1 -i]
+    arr[arr.length - 1 - i] = tempVal;
+  }
+  return arr
 }
 
 
@@ -16,7 +35,7 @@ function reverseArrayInPlace(arr) {
 
 
 // tests 
-console.log(reverseArray(["A", "B", "C"]));
+console.log(reverseArray(["A", "B", "D"]));
 // → ["C", "B", "A"];
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
